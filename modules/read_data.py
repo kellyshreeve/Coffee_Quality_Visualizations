17b7a5625a7ff5208ed71bfdc73c93463e9baaf9
kellyshreeve <kelly.shreeve@gmail.com>
2023-08-06 20:24:58 -0700

    Add geopandas

--- modules/read_data.py
+++ modules/read_data.py
@@ -2,4 +2,4 @@
 import pandas as pd
 
 # Import data
-coffee = pd.read_excel('/Users/kellyshreeve/Desktop/Data-Sets/df_arabica_clean.xlsx', header=0, index_col=0)
+coffee = pd.read_csv('/Users/kellyshreeve/Desktop/Data-Sets/df_arabica_clean.csv')
